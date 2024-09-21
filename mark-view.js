f0072d7d43229faa21b6761a745babee8352f020
Kanika Khurana <150192203+kanikakhurana5@users.noreply.github.com>
2024-09-21 23:33:24 +0530

    Delete mark-view.js

--- mark-view.js
+++
@@ -1,6 +0,0 @@
-// Add any JavaScript for interactivity here if needed
-// Example: For future button actions
-
-document.querySelector('.action-button').addEventListener('click', function() {
-    alert('Button clicked');
-});
